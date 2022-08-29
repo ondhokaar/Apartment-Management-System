8f9b00b673091e9d24fb59b0daeda1169bfa9488
shabbir <smzaman404@gmail.com>
2022-08-29 14:16:19 +0600

    fixing bill issue

--- AptMgmtSys/src/aptmgmtsys/utils/DocumentCreator.java
+++ AptMgmtSys/src/aptmgmtsys/utils/DocumentCreator.java
@@ -17,7 +17,7 @@ import javafx.scene.control.Label;
  */
 public class DocumentCreator {
 
-    public static boolean createInvoice(String deadline, double sumtotal, ObservableList<Node> children, String invoiceName) {
+    public static boolean createInvoice(String info, String deadline, int qty, double service, double other, String invoiceName) {
 
         PrintWriter fw = null;
 
@@ -25,26 +25,37 @@ public class DocumentCreator {
 
             fw = new PrintWriter("bills/" + invoiceName);
             BufferedWriter bw = new BufferedWriter(fw);
+            bw.write(info);
+            bw.newLine();
             
-
-            String service = "", amount = "";
-            int i = 0;
-            for (Node child : children) {
-                i++;
-                if(i == 3) {
-                    bw.write("--------------------------------------------------");
-                    bw.newLine();
-                }
-                if(i%2 == 1) {
-                    amount = (((Label) child).getText());
-                    continue;
-                }
-                bw.write((((Label) child).getText()) + "\t\t\t\t" + amount);
-                //bw.write((((Label) child).getText()) + " - " + i++);
-                bw.newLine();
-            }
-            bw.write("\n-------------------------------------------------");
-            bw.write("\nTotal Amount Payable :     [ " + sumtotal + " BDT ]");
+//****************************************************************************************
+//            String service = "", amount = "";
+//            int i = 0;
+//            for (Node child : children) {
+//                i++;
+//                if(i == 3) {
+//                    bw.write("--------------------------------------------------");
+//                    bw.newLine();
+//                }
+//                if(i%2 == 1) {
+//                    amount = (((Label) child).getText());
+//                    continue;
+//                }
+//                bw.write((((Label) child).getText()) + "\t\t\t\t" + amount);
+//                //bw.write((((Label) child).getText()) + " - " + i++);
+//                bw.newLine();
+//            }
+//*****************************************************************************************
+            bw.write("\n************************************************************\n");
+            bw.write(String.format(" %30s  : %10s", "Monthly Facility charge", ""+service));
+            //bw.write("Monthly Facility charge : \t" + service);
+            bw.newLine();
+            bw.write(String.format(" %30s  : %10s", "Other added charge", ""+other));
+            //bw.write("Other added charge : \t" + other);
+            bw.write("\n--------------------------------------------------------------------------\n");
+            double sumtotal = service + other;
+            bw.write(String.format(" %30s  : %10s", "Total Amount Payable", ""+service));
+            //bw.write("\nTotal Amount Payable :     [ " + sumtotal + " BDT ]");
             
             
             bw.write("\nPay on or before : " + deadline);
